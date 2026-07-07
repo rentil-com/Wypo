13157e96d886221498ffa647ff0b8e72f2dd67b5
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-07 15:22:27 +0200

    add (index.tsx) layout for login Page with Brand Logo

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -7,6 +7,7 @@ import { Header } from "@react-navigation/elements";
 import { ThemedView } from "./components/themed-view";
 import { ThemedText } from "./components/themed-text";
 import { Link } from 'expo-router';
+
 export default function LoginScreen() {
   const [login,setLogin] =  useState("")
   const [haslo,setHaslo] = useState("")
@@ -32,27 +33,50 @@ export default function LoginScreen() {
   return (
     
    <SafeAreaProvider style={styles.container}>
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.loginPage}>
+      {/* LEWA STRONA  */}
+        <View style={styles.leftSide}>
+          <View style={styles.heroImageWrapper}> 
+           <Image
+              source={require("../assets/logos/rentil_image.png")}
+              style={styles.heroImage}
+              resizeMode="contain"
+            />
+
+            </View>
+        </View>
+
+      {/* PRAWA STRONA */}
       <ThemedView style={styles.innerContainer} >
         <View style={styles.card}>
+            <View style={styles.cardTop}>
+                <Image
+                  source={require("../assets/logos/rentil.png")}
+                  style={styles.cardLogo}
+                  resizeMode="contain"
+                />
+
+      </View>
+
          <ThemedText type="title" style={styles.title} >Zaloguj sie</ThemedText>
 
      <View style={styles.socialContainer}>
-                    <TouchableOpacity> 
+                    <TouchableOpacity style={styles.socialButtonWrapper}> 
                     <Image source={require('../assets/icons/facebook-icon.png')} style={styles.socialButton}>
 
                     </Image>
                         </TouchableOpacity>
 
 
-                         <TouchableOpacity> 
+                         <TouchableOpacity style={styles.socialButtonWrapper}> 
                     <Image source={require('../assets/icons/apple-icon.png')} style={styles.socialButton}>
 
                     </Image>
-                        </TouchableOpacity>
+                        </TouchableOpacity >
 
 
-                         <TouchableOpacity> 
+                         <TouchableOpacity style={styles.socialButtonWrapper}> 
                     <Image source={require('../assets/icons/google-icon.png')} style={styles.socialButton}>
 
                     </Image>
@@ -74,8 +98,9 @@ export default function LoginScreen() {
         <ThemedText>Nie masz konta? <ThemedText type="link">Zaloz konto</ThemedText> </ThemedText>
       </Link>
       </View>
-      </ThemedView>
       
+      </ThemedView>
+       </View>
     </SafeAreaView>
    </SafeAreaProvider>
  
@@ -92,35 +117,41 @@ export default function LoginScreen() {
   },
   innerContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 56,
+    paddingVertical: 40,
+    backgroundColor: "#F8FBFF",
   },
   card: {
-    width: '100%',
-    maxWidth: 420,          
-    backgroundColor: '#ffffff',
+     width: "100%",
+    maxWidth: 520,
+    backgroundColor: "#FFFFFF",
     borderRadius: 32,
-    padding: 40,
-    paddingTop: 48,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 20 },
-    shadowOpacity: 0.12,
-    shadowRadius: 35,
-    elevation: 25,
+    paddingHorizontal: 52,
+    paddingVertical: 50,
+
+    shadowColor: "#0F172A",
+    shadowOffset: {
+    width: 0,
+    height: 24,
   },
+  shadowOpacity: 0.09,
+    shadowRadius: 40,
+    elevation: 20,
+},
   title: {
     fontSize: 32,
-    fontWeight: '700',
-    textAlign: 'center',
-    marginBottom: 32,
+    fontWeight: '900',
+    textAlign: 'left',
+    marginBottom: 28,
     color: '#0f172a',
   },
   socialContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    gap: 20,
-    marginBottom: 40,
+    gap: 14,
+    marginBottom: 34,
   },
   socialButton: {
     width: 64,
@@ -185,6 +216,67 @@ export default function LoginScreen() {
     marginTop: 28,
     alignItems: 'center',
   },
+  loginPage: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#F4F8FF",
+  },
+
+  leftSide: {
+    flex: 1.15,
+    backgroundColor: "#EEF6FF",
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  heroImage: {
+    width: "100%",
+    height: "100%",
+    minHeight: 760,
+    objectFit: "contain" as any,
+    objectPosition: "center center" as any,
+  },
+   cardTop: {
+  alignItems: "center",
+  justifyContent: "center",
+  gap: 16,
+  marginBottom: 34,
+},
+
+  cardLogo: {
+   width: 180,
+  height: 90,
+  },
+  socialButtonWrapper: {
+    flex: 1,
+    height: 54,
+    borderRadius: 14,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#DDE5F0",
+
+    shadowColor: "#0F172A",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 12,
+    elevation: 4,
+  },
+   safeArea: {
+    flex: 1,
+    backgroundColor: "#F4F8FF",
+  },
+  heroImageWrapper: {
+  width: "100%",
+  height: "92%",
+  justifyContent: "center",
+  alignItems: "center",
+},
 });
 
     
