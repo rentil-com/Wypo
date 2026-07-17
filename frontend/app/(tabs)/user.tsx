ca341a603eb00280f441a84b6997718e73a3a4e6
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-17 13:24:25 +0200

    add global state auth user

--- frontend/app/(tabs)/user.tsx
+++ frontend/app/(tabs)/user.tsx
@@ -117,6 +117,7 @@ export default function User() {
         
       return () => clearInterval(interval);
   },[lastResetDate])
+  
   return (
   
     <PageLayout> 
--- frontend/app/contexts/AuthContext.tsx
+++ frontend/app/contexts/AuthContext.tsx
@@ -2,20 +2,42 @@ import {
   createContext,
   useContext,
   useEffect,
+  useCallback,
   useState,
 } from "react";
 
 import type { ReactNode } from "react";
-
-import { login as LoginRequest, logout as LogoutRequest, confirm2FA as confirma2FARequest, getCurrentUser } from "@/services/auth.service";
+import { login as LoginRequest, logout as LogoutRequest, confirm2FA, confirm2FA as confirma2FARequest, getCurrentUser } from "@/services/auth.service";
 import { AuthStatus, SessionUser } from "@/types/auth";
+
+type AuthContextValue = {
+  status: AuthStatus;
+  user: SessionUser | null;
+  challenge: string | null;
+  error: string | null;
+  refreshSession: () => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+  verify2FA: (kod: string) => Promise<void>;
+};
+
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+const AuthContext = createContext<AuthContextValue | undefined>(
+  undefined
+);
+
+export function AuthProvider({children,} : AuthProviderProps) {
 
 const [status,setStatus] = useState<AuthStatus>("loading")
 const [user,setUser] = useState<SessionUser | null>(null)
 const [challenge,setChallenge] = useState<string | null>(null)
 const [error,setError] = useState<string |  null>(null)
 
-export async function refreshSession() {
+ const refreshSession = useCallback(async () => {
  setStatus("loading")
  setError(null)
 
@@ -31,4 +53,107 @@ export async function refreshSession() {
     setError(error instanceof Error ? error.message : "Nieznany błąd")
  }
     
+},[])
+
+
+ async function signIn(email : string , password : string) {
+    setStatus("loading")
+    setError(null)
+    setChallenge(null);
+
+    try {
+        const response = await LoginRequest(email, password)
+        if("requires_2fa" in response){
+            setChallenge(response.challenge)
+            setStatus("awaiting_2fa")
+        }
+        else {
+            setUser(response.user)
+            setStatus("authenticated")
+        }
+    }
+    catch(error) {
+        setUser(null)
+    setStatus("anonymous")
+     setChallenge(null);
+
+    setError(error instanceof Error ? error.message : "Nieznany błąd")
+    }
+}
+
+
+
+ async function signOut() {
+    setStatus("loading")
+    setError(null)
+    try {
+        await LogoutRequest()
+        setUser(null)
+        setChallenge(null)
+        setStatus("anonymous")
+    }
+    catch(error) {
+        setStatus("authenticated");
+
+        setError(error instanceof Error ? error.message : "Nieznany błąd")
+    }
+
+    
+}
+
+
+ async function verify2FA( kod : string) {
+    setStatus("loading")
+    setError(null)
+    if (!challenge) {
+  setStatus("anonymous");
+  setError("Brak aktywnego logowania 2FA");
+  return;
+}
+    try {
+       
+
+        const response = await confirma2FARequest(challenge,kod)
+        setUser(response.user)
+        setChallenge(null)
+        setStatus("authenticated")
+        
+    }
+    catch(error) {
+        setStatus("awaiting_2fa");
+        setError(error instanceof Error ? error.message : "Nieznany błąd")
+    }
+    
+}
+useEffect(()=> {
+ void refreshSession()   
+},[refreshSession])
+ return (
+    <AuthContext.Provider
+      value={{
+        status,
+        user,
+        challenge,
+        error,
+        refreshSession,
+        signIn,
+        signOut,
+        verify2FA,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+ )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuth musi być użyty wewnątrz AuthProvider"
+    );
+  }
+
+  return context;
 }
