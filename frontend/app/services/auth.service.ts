336f582a598c9ec5a72f628a683643670478586b
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-20 11:59:07 +0200

    add (auth) register, registerConfirm service

--- frontend/app/services/auth.service.ts
+++ frontend/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { apiPost, apiGet } from "./api";
 
-import { LoginBody,AuthResponse, LogoutResponse, Confirm2FABody, LoginSuccessResponse, AccountDetails } from "@/types/auth";
+import { LoginBody,AuthResponse, LogoutResponse, Confirm2FABody, LoginSuccessResponse, AccountDetails, AccountCreate, AccountCreateConfirm, AccountCreateResponse, AccountCreateSuccessResponse } from "@/types/auth";
 
 
 export async function login(email : string, password : string) {
@@ -47,4 +47,44 @@ export async function getCurrentUser() {
   const response = await apiGet("/account/details");
 
   return response as AccountDetails;
+}
+
+
+export async function register(imie : string, nazwisko : string, email : string, password : string) {
+    const poprawnyEmail = email.trim().toLowerCase();
+    const poprawneImie = imie.trim()
+    const poprawneNazwisko = nazwisko.trim()
+    if (!poprawnyEmail || !password || !poprawneImie || !poprawneNazwisko) {
+        throw new Error("wszystkie pola  są wymagane");
+    }
+    const body : AccountCreate = {
+        imie : poprawneImie,
+        nazwisko : poprawneNazwisko,
+        email : poprawnyEmail,
+        password : password
+    }
+    const response = await apiPost("/account/create",body)
+    return response as AccountCreateResponse
+}
+
+export async function registerConfirm(email: string, kod : string) {
+    const poprawnyEmail = email.trim().toLowerCase();
+
+    if (!poprawnyEmail ) {
+        throw new Error("Email jest wymagany");
+    }
+
+   if(!/^[0-9]{6}$/.test(kod) || kod.length !=6){
+    throw new Error("Kod musi sie skladac tylko z 6 cyfr")
+   }
+
+    const body : AccountCreateConfirm  = {
+        email : poprawnyEmail,
+        kod : kod,
+    }
+  
+
+
+    const response = await apiPost("/auth/register-confirm",body) 
+      return response as AccountCreateSuccessResponse   
 }
