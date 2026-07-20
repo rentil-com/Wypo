d8abee30e74b9205325c58120b20ae25bcd4a636
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-20 11:41:37 +0200

    add (auth.ts) types for Register (account/create)

--- frontend/app/types/auth.ts
+++ frontend/app/types/auth.ts
@@ -57,3 +57,29 @@ export type AuthStatus =
   | "anonymous"
   | "awaiting_2fa"
   | "authenticated";
+
+
+export type AccountCreate ={
+  imie : string,
+  nazwisko : string,
+  email : string
+  password : string,
+}
+
+
+export type AccountCreateResponse = {
+  message : string,
+  expires_in : number,
+  max_attempts : number
+}
+
+
+export type AccountCreateConfirm = {
+  email : string, 
+  kod : string
+}
+
+export type AccountCreateSuccessResponse = {
+  message : string,
+  user : AccountDetails
+}
