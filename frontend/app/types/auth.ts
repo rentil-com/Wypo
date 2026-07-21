8f19b9a740344ce64ae3d44d1afe141702f43116
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-21 12:27:43 +0200

    add (auth.ts) types for auth/password/reset and auth/password/resert/confirm

--- frontend/app/types/auth.ts
+++ frontend/app/types/auth.ts
@@ -109,3 +109,24 @@ export type EmailChangeConfirmResponse = {
   message: string;
   email: string;
 };
+
+export type PasswordResetBody = {
+  email: string;
+};
+
+export type PasswordReset = {
+  message : string,
+  challenge : string,
+  expires_in : string,
+  max_attempts : string
+}
+
+export type PasswordResetConfirm = {
+  challenge : string,
+  code : string,
+  password : string
+}
+
+export type PasswordResetConfirmResponse = {
+  message: string;
+};
