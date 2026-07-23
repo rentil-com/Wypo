742573122de72fa3c4a02335988d8115f052dfb3
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-23 11:05:42 +0200

    add (reviews/recenzje) add types

---
+++ frontend/src/features/reviews/reviews.types.ts
@@ -0,0 +1,29 @@
+export type ReviewsStatus = "aktywna" |
+"ukryta" | "usunieta"
+
+export type  Review_Rating = 1 | 2 | 3 | 4 | 5
+
+export type ReviewResponse=  {
+    id : number,
+    uzytkownik_id : number,
+    sprzet_id : number,
+    wypozyczenie_id : number | null,
+    gwiazdki : Review_Rating
+    tresc : string | null,
+    status : ReviewsStatus,
+    data_dodania : string,
+    imie? : string,
+    nazwisko? : string,
+    nazwa_sprzetu? : string
+}
+
+export type ProductReviewsResponse = {
+  strona: number;
+  limitRecenzjiNaStrone: number;
+  sprzet_id: number;
+  srednia_ocen: number;
+  liczba_recenzji: number;
+  total: number;
+  liczbaStron: number;
+  dane: ReviewResponse[];
+};
