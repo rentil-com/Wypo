62f984f1d51d6f60986d4d2e429e513f56224e70
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-23 13:00:27 +0200

    add (categories.management.types.ts) admin category types

---
+++ frontend/src/features/categories/categories.management.types.ts
@@ -0,0 +1,11 @@
+export type AddCategoryResponse = {
+    id : number,
+    nazwa : string,
+    zdjecie_url : string | null,
+}
+
+export type EditCategoryResponse = {
+    id : number,
+    nazwa : string, 
+    zdjecie_url : string | null,
+}
