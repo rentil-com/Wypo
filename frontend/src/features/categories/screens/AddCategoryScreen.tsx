052a146594826efafcfa1ff05b321a7407ce9a7c
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-07-23 13:37:48 +0200

    add (AddCategoryScreen) admin addCategory service implementation

--- frontend/src/features/categories/screens/AddCategoryScreen.tsx
+++ frontend/src/features/categories/screens/AddCategoryScreen.tsx
@@ -10,11 +10,16 @@ import {
 
 import { useAuth } from "@/contexts/AuthContext";
 import PageLayout from "@components/shared/Layout/PageLayout";
-
+import { useState } from "react";
 import styles from "./CategoryAdminForm.styles";
-
+import { dodajKategorie } from "../categories.management.services";
+import * as ImagePicker from "expo-image-picker";
 export default function AddCategoryScreen() {
   const { status, user } = useAuth();
+  const [nowaNazwa, setNowaNazwa] = useState("")
+  const [zdjecie,setZdjecie] = useState<ImagePicker.ImagePickerAsset | null>(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   if (status === "loading") {
     return (
@@ -26,6 +31,58 @@ export default function AddCategoryScreen() {
 
   if (status !== "authenticated" || user?.rola !== "admin") {
     return <Redirect href="/(tabs)/user" />;
+  }
+  const wybierzZdjecie = async ()=>{
+    setError(null)
+    const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ["images"],
+    allowsEditing: true,
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    setZdjecie(result.assets[0]);
+  }
+  }
+
+
+  const dodaj = async () => {
+    setError(null)
+    const poprawnaNazwa = nowaNazwa.trim()
+    if(!poprawnaNazwa ) {
+      setError("Nazwa nie moze byc pusta")
+      return;
+    }
+    if (poprawnaNazwa.length > 100) {
+    setError("Nazwa może mieć maksymalnie 100 znaków");
+    return;
+    }
+
+       setLoading(true)
+
+     try{
+      const formData = new FormData();
+      formData.append("nazwa",poprawnaNazwa)
+      if(zdjecie?.file){
+          formData.append("zdjecie",zdjecie.file)
+      }
+      else if (zdjecie){
+        formData.append("zdjecie", {uri : zdjecie.uri,
+          name : zdjecie.fileName ?? "kategoria.jpg",
+          type : zdjecie.mimeType ?? "image/jpeg"
+      }as any,)
+      }
+      
+      await dodajKategorie(formData)
+      router.back()
+    }
+    catch(error){
+      setError(error instanceof Error  ? error.message : "Nie udalo sie dodac kategorii ")
+    }
+    finally {
+      setLoading(false)
+    }
+    
   }
 
   return (
@@ -48,6 +105,8 @@ export default function AddCategoryScreen() {
           <View style={styles.field}>
             <Text style={styles.fieldLabel}>Nazwa kategorii</Text>
             <TextInput
+              value={nowaNazwa}
+              onChangeText={(val)=> setNowaNazwa(val)}
               style={styles.input}
               placeholder="Np. Sprzęt ogrodowy"
               placeholderTextColor="#94A3B8"
@@ -56,7 +115,7 @@ export default function AddCategoryScreen() {
 
           <View style={styles.field}>
             <Text style={styles.fieldLabel}>Zdjęcie kategorii</Text>
-            <Pressable style={styles.imagePicker}>
+            <Pressable style={styles.imagePicker} onPress={()=> wybierzZdjecie()}>
               <View style={styles.imagePickerIcon}>
                 <MaterialIcons name="add-photo-alternate" size={27} color="#2563EB" />
               </View>
@@ -65,6 +124,7 @@ export default function AddCategoryScreen() {
                 Plik PNG, JPG lub WEBP.
               </Text>
             </Pressable>
+            
           </View>
 
           <View style={styles.formActions}>
@@ -75,7 +135,7 @@ export default function AddCategoryScreen() {
               <Text style={styles.cancelButtonText}>Anuluj</Text>
             </Pressable>
 
-            <Pressable style={[styles.actionButton, styles.submitButton]}>
+            <Pressable style={[styles.actionButton, styles.submitButton]} onPress={()=>dodaj()}>
               <MaterialIcons name="add" size={19} color="#FFFFFF" />
               <Text style={styles.submitButtonText}>Dodaj kategorię</Text>
             </Pressable>
